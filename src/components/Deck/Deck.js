079f5c701cc8943b5abac56ae018a1dc64bce3ac
Jason Stankevich <jason.stankevich@gmail.com>
2020-11-19 16:39:02 -0500

    card construct change

--- src/components/Deck/Deck.js
+++ src/components/Deck/Deck.js
@@ -1,19 +1,5 @@
-const SUITS = ['♥', '♦', '♠', '♣'];
-const VALUES = [
-  'A',
-  '2',
-  '3',
-  '4',
-  '5',
-  '6',
-  '7',
-  '8',
-  '9',
-  '10',
-  'J',
-  'Q',
-  'K',
-];
+const SUITS = ['h', 'd', 's', 'c'];
+const VALUES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
 export default class Deck {
   constructor(cards = createDeck()) {
