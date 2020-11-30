d91db5ce24977120bd3537ce9835bf0b886deba9
Harrison Winkler <harryewinkler@thinkful.com>
2020-11-30 15:21:50 -0500

    more styling changes

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -7,12 +7,17 @@ import './LandingPage.css';
 export default function LandingPage() {
   
     return (
-      <Section className="container">
-          <h1>Welcome to the Landing Page!</h1>
-          <p>Ahoy there! Welcome to GoFish.io.</p>
+      <Section>
+        <div className="container">
+
+          <h1>Ahoy there!</h1>
+          <p>Welcome to GoFish.io.</p>
           <p>Here you can play the classic card game “Go Fish” online with up to 3 other friends in an online environment.</p>
           <p>GoFish.io is a collaborative product put together by Caleb, Harry, Jason, Malik, Michael, and Jason.</p>
+          <p>.</p>
           <Link to="/register">Register</Link>
+        </div>
+
       </Section>
     );
   
