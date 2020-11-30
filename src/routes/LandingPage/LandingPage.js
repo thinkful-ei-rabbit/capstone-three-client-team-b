beda8d9b0fbf219566802007db7db87f76330d72
Harrison Winkler <harryewinkler@thinkful.com>
2020-11-30 13:39:51 -0500

    in progress

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import { Button, Section } from '../../components/Utils/Utils';
 import { Link } from 'react-router-dom';
+import Logo from '../../components/Logo/Logo'
 
 import './LandingPage.css';
 
-export default class LandingPage extends Component {
-  render() {
+export default function LandingPage(props) {
+  
     return (
-      <Section>
-        <h1>Login!</h1>
+      <Section className="body">
+        <div className="container">
+          <Logo />
+          <h1>welcome to the landing page</h1>
+          <Link to="/register">Register</Link>
+        </div>
+
       </Section>
     );
-  }
+  
 }
