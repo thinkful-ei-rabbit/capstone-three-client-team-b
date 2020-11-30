d8a9e18509b5e11a9694b804e533d8b2bb47d5ea
Harrison Winkler <harryewinkler@thinkful.com>
2020-11-30 15:05:04 -0500

    landing page styles

--- src/routes/LandingPage/LandingPage.js
+++ src/routes/LandingPage/LandingPage.js
@@ -1,20 +1,18 @@
 import React, { Component } from 'react';
 import { Button, Section } from '../../components/Utils/Utils';
 import { Link } from 'react-router-dom';
-import Logo from '../../components/Logo/Logo'
 
 import './LandingPage.css';
 
-export default function LandingPage(props) {
+export default function LandingPage() {
   
     return (
-      <Section className="body">
-        <div className="container">
-          <Logo />
-          <h1>welcome to the landing page</h1>
+      <Section className="container">
+          <h1>Welcome to the Landing Page!</h1>
+          <p>Ahoy there! Welcome to GoFish.io.</p>
+          <p>Here you can play the classic card game “Go Fish” online with up to 3 other friends in an online environment.</p>
+          <p>GoFish.io is a collaborative product put together by Caleb, Harry, Jason, Malik, Michael, and Jason.</p>
           <Link to="/register">Register</Link>
-        </div>
-
       </Section>
     );
   
