599d65f4b64b76c04c3356bdf9c6d48ff38a8ac8
Harrison Winkler <harryewinkler@thinkful.com>
2020-12-01 11:58:25 -0500

    logo size fixed

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,7 +5,7 @@ import logo from './logo.png';
 export default function Logo() {
   return (
     <div className="logo-container">
-      <img src={logo} alt="logo" />
+      <img src={logo} alt="logo" style={{width: 100}}/>
       <span className="logo-text">GoFish.io</span>    
     </div>
   );
